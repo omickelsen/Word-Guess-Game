8c17041ae3f7b62e0b8875b095998f1dcd0b7970
omickelsen <omickelsen@gmail.com>
2019-02-19 15:42:10 -0700

    The initial workings of the Psychic game and first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+//letter choices that are available
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+console.log("computer choice: " + computerChoices);
+
+// setting up the wins to 0 and the losses to 0 and the guesses amount
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessedLetters = [];
+
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var guessedLettersText = document.getElementById("guessedLetters-text");
+
+//formula for user guess
+
+document.onkeyup = function(event) {
+    var userGuess = event.key
+console.log("user guess: " + userGuess);
+//Computer has a random guess
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("computer guess: " + computerGuess);
+
+if ((userGuess === computerGuess)){
+    wins++;
+}
+
+
+
+directionsText.textContent = "";
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+guessedLettersText.textContent = "Guesses Made: " + guessedLetters;
+
+
+
+
+};
