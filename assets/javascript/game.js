c1f50007a161bb6600814f05e020f84d6b183d04
omickelsen <omickelsen@gmail.com>
2019-02-19 16:37:33 -0700

    added text on the html for Wins, Losses, Guesses Left.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //letter choices that are available
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 console.log("computer choice: " + computerChoices);
 
@@ -15,38 +15,38 @@ var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesLeft-text");
 var guessedLettersText = document.getElementById("guessedLetters-text");
 //reset function
-var reset = function() {
-guessesLeft = 9;
-guessedLetters = [];
+var reset = function () {
+    guessesLeft = 9;
+    guessedLetters = [];
 }
 //formula for user guess
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var userGuess = event.key
-console.log("user guess: " + userGuess);
-//Computer has a random guess
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log("computer guess: " + computerGuess);
+    console.log("user guess: " + userGuess);
+    //Computer has a random guess
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("computer guess: " + computerGuess);
 
-if ((guessesLeft > 0) && (userGuess != computerGuess)) {
-    guessesLeft--;
-    guessedLetters++;
-} else if (userGuess === computerGuess){
-    wins++;
-    reset();
-} else {
-    losses++;
-    reset();
-}
-
-
+    if ((guessesLeft > 0) && (userGuess != computerGuess)) {
+        guessesLeft--;
+        guessedLetters++;
+    } else if (userGuess === computerGuess) {
+        wins++;
+        reset();
+    } else {
+        losses++;
+        reset();
+    }
 
 
-directionsText.textContent = "Guess what letter I'm thinking of";
-winsText.textContent = "Wins: " + wins;
-lossesText.textContent = "Losses: " + losses;
-guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-guessedLettersText.textContent = "Guesses Made: " + guessedLetters;
+
+
+    directionsText.textContent = "Guess what letter I'm thinking of";
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    guessedLettersText.textContent = "Guesses Made: " + guessedLetters;
 
 
 
