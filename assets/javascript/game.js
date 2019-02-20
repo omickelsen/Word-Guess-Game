56b079a44fe96660b60d26e86b5945daafabf36a
omickelsen <omickelsen@gmail.com>
2019-02-19 23:43:04 -0700

    lots of work done fo a commit on the word guess game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,147 @@
-//letter choices that are available
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Made the variables first to use in the functions
+//Array of fruitwords
+var randomWords = ['butter', 'grandpa', 'television', 'sclouns', 'hangman', 'love', 'tacos', 'pneumonoultramicroscopicsilicovolcanoconiosis',
+    'texas', 'utah', 'california', 'halo', 'apex', 'predator', 'evil', 'vaccine',
+    'doctor', 'elk', 'gorilla', 'lion', 'bottle', 'crossfit', 'javascript', 'xbox', 'nintendo', 'baloon',
+    'backpack', 'chair', 'trump'
+];
+var blanksAndSuccess = []; //Correct guesses and blanks will be in this array
+var blanks = 0; //holds blanks spaces for the word chosen
+var currentWord = "";
+var currentLetters = [];
+// Counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 12;
 
-console.log("computer choice: " + computerChoices);
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
 
-// setting up the wins to 0 and the losses to 0 and the guesses amount
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var guessedLetters = [];
-console.log(typeof (guessedLetters))
+//array for users guessed letters
+var guessedLetter = [];
+//array for users correct guesses
+var correctLetter = [];
+//guessed letters that are incorrect
+var incorrectGuess = [];
 
-var directionsText = document.getElementById("directions-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
-var guessedLettersText = document.getElementById("guessedLetters-text");
-//reset function
-var reset = function () {
-    guessesLeft = 9;
-    guessedLetters = [];
+
+//Below is the functionality of the game
+function startGame() {
+    numGuesses = 12;
+    blanksAndSuccess = [];
+    guessedLetter = [];
+    incorrectGuess = [];
+    //Selects a fruitword at random
+    currentWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+
+    //Splits the current word into letters, so that you can match the users guesses to the letters of the word
+    currentLetters = currentWord.split("");
+
+    //Need to know how many blanks
+    blanks = currentLetters.length;
+    for (var i = 0; i < blanks; i++) {
+        blanksAndSuccess.push("_")
+    }
+    console.log(currentWord);
+    document.getElementById('currentWord').innerHTML = "Find the missing letters: " + blanksAndSuccess.join(" ");
+    document.getElementById('guessesRemaining').innerHTML = "Guesses left: " + numGuesses;
+    document.getElementById('guessed').innerHTML = "Letters already guessed: "
+        // document.getElementsByClassName('btn-primary').disabled = false;
+    console.log("Start");
+    buttons();
 }
-//formula for user guess
 
-document.onkeyup = function (event) {
-    var userGuess = event.key
-    console.log("user guess: " + userGuess);
-    //Computer has a random guess
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log("computer guess: " + computerGuess);
-    // the if else statments with the added .push to count the letters in the array.
-    if ((guessesLeft > 0) && (userGuess != computerGuess)) {
-        guessesLeft--;
-        guessedLetters.push(userGuess);
-        console.log(typeof (guessedLetters));
-    } else if (userGuess === computerGuess) {
-        wins++;
-        console.log("winner")
-        reset();
+
+// create alphabet ul
+function buttons() {
+    //gets the alphabet button id from html
+    var myButtons = document.getElementById('alphabet-btns');
+    document.getElementById('alphabet-btns').innerHTML = '';
+    //loops through the alphabet
+    for (var i = 0; i < alphabet.length; i++) {
+        //creates li for list
+        let listItem = document.createElement('li');
+        //this creates the button and adds the bootstrap button look to it
+        listItem = document.createElement('BUTTON');
+        listItem.classList.add('btn-primary');
+        //gives each list item the id letter
+        listItem.id = 'letters';
+        listItem.innerHTML = alphabet[i];
+        //appends listIem to my buttons
+        myButtons.appendChild(listItem);
+        listItem.dataset.alphabet = alphabet[i];
+
+        listItem.onclick = function() {
+            this.disabled = true;
+            var userGuess = listItem.dataset.alphabet;
+            guessedLetter.push(userGuess);
+            document.getElementById('guessed').innerHTML = "Letters Already Guessed: " + guessedLetter.join(" ");
+            checkLetters(userGuess); // runs the code to check for correctness      
+            round();
+        }
+
+    }
+}
+
+
+//Checks if users letter is in the word
+function checkLetters(letter) {
+    //using Boolean to check if the letter is in the word
+    var letterInWord = false;
+    //loop that goes through the length of the word
+    for (var i = 0; i < blanks; i++) {
+        if (currentWord[i] == letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+
+            if (currentWord[i] == letter) {
+                blanksAndSuccess[i] = letter
+            }
+        }
+        console.log(blanksAndSuccess);
+
     } else {
-        losses++;
-        console.log("loser")
-        reset();
+        incorrectGuess.push(letter);
+        numGuesses--;
+        console.log("that was incorrect " + numGuesses + " are remaining");
+    }
+}
+
+//Upon finishing
+function round() {
+    console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+
+    // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
+    document.getElementById("guessesRemaining").innerHTML = "Number Of Guesses Remaining: " + numGuesses;
+    document.getElementById("currentWord").innerHTML = "Find the missing letters: " + blanksAndSuccess.join(" ");
+    document.getElementById("guessed").innerHTML = "Letters already guessed: " + incorrectGuess.join(" ");
+
+    // If we have gotten all the letters to match the solution... 
+    if (currentLetters.toString() == blanksAndSuccess.toString()) {
+        winCounter++; // add to the win counter 
+        document.getElementById("word").innerHTML = "The last word was " + currentWord;
+        //TODO set up event.preventDefault() on alert so that the current word is shown on the game screen
+        alert("You win! The word was " + currentWord); // give the user an alert   
+        // Update the win counter in the HTML
+        document.getElementById("winCounter").innerHTML = "You have won " + winCounter + " game(s)";
+        startGame(); // restart the game 
     }
 
+    // If we've run out of guesses
+    else if (numGuesses == 0) {
+        lossCounter++; // add to the loss counter 
+        document.getElementById("word").innerHTML = "The last word was " + currentWord;
+        alert("You lose. The word was " + currentWord); // gives the user an alert
+        // Update the loss counter in the HTML
+        document.getElementById("lossCounter").innerHTML = "You have lost " + lossCounter + " game(s)";
+        startGame(); // restart the game
+    }
+}
 
-
-
-    directionsText.textContent = "Guess what letter I'm thinking of";
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-    guessedLettersText.textContent = "Guesses Made: " + guessedLetters;
-
-
-
-
-};
+//Calling the startGame function
+document.getElementById("gameStart").onclick = function() { buttons() };
+startGame();
